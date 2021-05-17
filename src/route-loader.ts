cd5767f61f57d3121cf7456ead776ad2d420759f
Choooks22 <49726759+Choooks22@users.noreply.github.com>
2021-05-17 20:44:50 +0800

    Add function to normalize path on windows systems

--- src/route-loader.ts
+++ src/route-loader.ts
@@ -1,16 +1,20 @@
 import { Handler, Router } from 'express';
-import { dirname, join } from 'path';
+import { dirname, join, posix, sep, win32 } from 'path';
 import { parseMiddleware } from './middleware';
 import { Method } from './types';
 
 const METHODS = Object.freeze<Method>(['get', 'post', 'put', 'patch', 'del', 'all']);
+
+const normalizePath: (path: string) => string = sep === win32.sep
+  ? path => path.replace(/\\/g, posix.sep)
+  : path => path;
 
 export const createRouteLoader = (basedir: string, router: Router, strict: boolean) => {
   return (filename: string, filepath: string) => {
     const routepath = dirname(filepath.slice(basedir.length)); // trim extra directories from route
     const routename = filename.startsWith('index') ? '' : filename.replace(/\..*$/, ''); // parse proper filename
 
-    const route = join(routepath, routename);
+    const route = normalizePath(join(routepath, routename));
     const handlers = Object.entries<Handler>(require(filepath));
 
     // find any middlewares first before looping through each handler
