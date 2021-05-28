739fc9e990b7ac93e544f2d8dff09a598db12f8a
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-05-28 16:49:08 +0800

    Extract file system traversing to dedicated file

---
+++ src/traverse.ts
@@ -0,0 +1,19 @@
+import { lstatSync, readdirSync } from 'fs';
+import { basename, join } from 'path';
+import { Loader } from 'route-loader';
+
+export function traverse(this: { load: Loader }, path: string, checkDir = false) {
+  if (checkDir && !lstatSync(path).isDirectory()) {
+    return this.load(basename(path), path);
+  }
+
+  for (const filename of readdirSync(path)) {
+    const filepath = join(path, filename);
+
+    if (lstatSync(filepath).isDirectory()) {
+      return traverse.call(this, filepath);
+    }
+
+    this.load(filename, filepath);
+  }
+}
